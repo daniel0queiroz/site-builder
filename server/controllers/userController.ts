48cc7db740be858e05573547ac71a09427497217
daniel0queiroz <daniel.olivqueiroz@outlook.com>
2026-05-20 16:44:16 -0300

    feat: enhance Settings page with Change Password and Delete Account features; add user authentication middleware and user credit management

---
+++ server/controllers/userController.ts
@@ -0,0 +1,81 @@
+// Get User Credits
+
+import { Request, Response } from "express";
+import prisma from "../lib/prisma.js";
+
+export const getUserCredits = async (req: Request, res: Response) => {
+  try {
+    const userId = req.userId;
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    res.json({ credits: user?.credits });
+  } catch (error: any) {
+    console.log(error.code || error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Controller Function to create New Project
+
+export const createUserProject = async (req: Request, res: Response) => {
+  const userId = req.userId;
+  try {
+    const { initial_prompt } = req.body;
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (user && user.credits < 5) {
+      return res
+        .status(403)
+        .json({ message: "add credits to create more projects" });
+    }
+
+    // Create a new project
+    const project = await prisma.websiteProject.create({
+      data: {
+        name:
+          initial_prompt.length > 50
+            ? initial_prompt.substring(0, 47) + "..."
+            : initial_prompt,
+        initial_prompt,
+        userId,
+      },
+    });
+
+    // Update User's Total Creation
+
+    await prisma.user.update({
+      where: { id: userId },
+      data: { totalCreation: { increment: 1 } },
+    });
+
+    await prisma.conversation.create({
+      data: {
+        role: "user",
+        content: initial_prompt,
+        projectId: project.id,
+      },
+    });
+
+    await prisma.user.update({
+      where: { id: userId },
+      data: { credits: { decrement: 5 } },
+    });
+
+    res.json({ projectId: project.id });
+  } catch (error: any) {
+    console.log(error.code || error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
